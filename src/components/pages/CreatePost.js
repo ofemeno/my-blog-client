8d813707c8f0ce8eabf99bcf229533e51177357a
Ofem <enofofem-development@yahoo.com>
2023-10-27 13:57:44 +0100

    add file

--- src/components/pages/CreatePost.js
+++ src/components/pages/CreatePost.js
@@ -1,14 +1,65 @@
-import ReactQuill from 'react-quill';
-// import 'react-quill/dist/quill.snow.css';
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
   return (
     <>
-      <form action="">
-        <input type="text" placeholder={"title"} />
-        <input type="text" placeholder={"summary"} />
+      <form action="" className="create">
+        <input
+          type="text"
+          placeholder={"title"}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder={"summary"}
+          value={summary}
+          onChange={(e) => setSummary(e.target.summary)}
+        />
         <input type="file" />
-        <ReactQuill />
+        <ReactQuill
+          value={content}
+          modules={modules}
+          formats={formats}
+          onChange={(newValue) => setContent(newValue)}
+        />
+        <button>Create Post</button>
       </form>
     </>
   );
